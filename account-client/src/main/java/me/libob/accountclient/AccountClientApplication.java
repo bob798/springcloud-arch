0aa7a7154ec19657d7cc04e56874ed99b569fac8
bob <bob.liu@happyelements.net>
2022-01-29 11:31:28 +0800

    init

---
+++ account-client/src/main/java/me/libob/accountclient/AccountClientApplication.java
@@ -0,0 +1,13 @@
+package me.libob.accountserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountClientApplication.class, args);
+    }
+
+}
---
+++ platform-gateway/src/main/java/me/libob/platformgateway/PlatformGatewayApplication.java
@@ -0,0 +1,13 @@
+package me.libob.platformgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformGatewayApplication.class, args);
+    }
+
+}
